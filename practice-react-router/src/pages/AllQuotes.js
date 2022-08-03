1b6365459f5aa6021802f3be5e820e2bd00a4704
Jhom <jhomwan238@gmail.com>
2022-08-03 20:52:25 +0700

    adding dummy quotes

--- practice-react-router/src/pages/AllQuotes.js
+++ practice-react-router/src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Frank', text: 'Learning is great!'},
+    {id: 'q2', author: 'Jhom', text: 'Learning gets you better places.'},
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes</h1>
+    return <QuoteList quotes={DUMMY_QUOTES}/>;
 };
 
 export default AllQuotes;
--- practice-react-router/src/pages/QuoteDetail.js
+++ practice-react-router/src/pages/QuoteDetail.js
@@ -1,16 +1,27 @@
 import React, { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'Frank', text: 'Learning is great!'},
+  {id: 'q2', author: 'Jhom', text: 'Learning gets you better places.'},
+];
 
 const QuoteDetail = () => {
-  const quote = useParams();
+  const params = useParams();
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if(!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <h2>{quote.quoteId}</h2>
-      <Route path={`/quotes/:${quote.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <Route path={`/quotes/:${params.quoteId}/comments`}>
         <Comments/>
       </Route>
     </Fragment>
