bc44a71c09e8fab79ac40fcb34483e4e27cd0729
Jhom <jhomwan238@gmail.com>
2022-08-04 23:03:21 +0700

    sending and getting quote via http

--- practice-react-router/src/pages/NewQuote.js
+++ practice-react-router/src/pages/NewQuote.js
@@ -1,19 +1,28 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import QuoteForm from '../components/quotes/QuoteForm';
+import useHttp from '../hooks/use-http';
+import {addQuote} from '../lib/api';
 
 const NewQuote = () => {
+    const {sendRequest, status} = useHttp(addQuote);
     const history = useHistory();
 
+    useEffect(() => {
+        if(status === 'completed'){
+            history.push('/quotes');
+        }
+    }, [status, history]);
+
     const addQuoteHandler = quoteData => {
-        history.push('/quotes') //use can go back with <- arrow
+        sendRequest(quoteData);
     };
 
     return (
         <Fragment>
             <h1>New Quote</h1>
-            <QuoteForm onAddQuote={addQuoteHandler}/>
+            <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>
         </Fragment>
     );
 };
--- practice-react-router/src/pages/QuoteDetail.js
+++ practice-react-router/src/pages/QuoteDetail.js
@@ -1,27 +1,41 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
-
-const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Frank', text: 'Learning is great!'},
-  {id: 'q2', author: 'Jhom', text: 'Learning gets you better places.'},
-];
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from '../hooks/use-http';
+import {getSingleQuote} from '../lib/api';
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const {quoteId} = params;
 
-  if(!quote) {
+  const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  },[sendRequest, quoteId]);
+
+  if(status === 'pending'){
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+
+  if(error){
+    return <p className="centered">{error}</p>
+  }
+
+  if(!loadedQuote.text) {
     return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
       <Route path={match.path} exact>
         <div className='centered'>
           <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
