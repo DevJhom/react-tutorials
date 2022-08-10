23af96dd5050a193135353e7faa2368923e4260c
Jhom <jhomwan238@gmail.com>
2022-08-10 22:20:44 +0700

    redirecting and logout functions

--- authentication-project/src/components/Auth/AuthForm.js
+++ authentication-project/src/components/Auth/AuthForm.js
@@ -1,6 +1,7 @@
 import { useState, useRef, useContext } from "react";
-import AuthContext from "../../store/auth-context";
+import { useHistory } from 'react-router-dom';
 
+import AuthContext from "../../store/auth-context";
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
@@ -8,6 +9,7 @@ const AuthForm = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -57,6 +59,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace('/');
       })
       .catch((err) => {
         alert(err.message);
