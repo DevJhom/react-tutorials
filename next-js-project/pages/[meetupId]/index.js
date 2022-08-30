7cb1f2ad41529eedb09cc12b6dc27029bef758d3
Jhom <jhomwan238@gmail.com>
2022-08-30 21:47:49 +0700

    pre-render page with Static Site Generation

---
+++ next-js-project/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1582192730841-2a682d7375f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1NzkxNDE0OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+      title="First Meetup"
+      address="Hokaido, Japan"
+      description="This is a first meeting"
+    />
+  );
+};
+
+export default MeetupDetails;
