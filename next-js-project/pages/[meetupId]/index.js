8a2dbe53f0b9719ad0605dd340e9e6f1b033a415
Jhom <jhomwan238@gmail.com>
2022-09-06 21:35:38 +0700

    getting to know getStaticPaths()

--- next-js-project/pages/[meetupId]/index.js
+++ next-js-project/pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ const MeetupDetails = () => {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.unsplash.com/photo-1582192730841-2a682d7375f9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1NzkxNDE0OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Hokaido, Japan",
+        description: "This is a first meeting",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
--- next-js-project/pages/index.js
+++ next-js-project/pages/index.js
@@ -26,7 +26,22 @@ const HomePage = (props) => {
   );
 };
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   //fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// }
+
 export async function getStaticProps() {
+// fetch data from an API
+
   return {
     props: {
       meetups: DUMMY_MEETUPS
